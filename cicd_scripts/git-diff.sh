a553f29e25f0aae43d026ce0d994fa034045051f
drsalas_indra <drsalas@minsait.com>
2024-07-17 14:05:26 +0200

    tests

--- cicd_scripts/git-diff.sh
+++ cicd_scripts/git-diff.sh
@@ -10,7 +10,7 @@ for file in $CHANGED_FILES; do
   if [[ "$file" == *".cbl"* ]]; then
     BINARY_NAME=$(basename "$file" .cbl)
     EXISTS=$(yq e ".binaries [] | select(.name==\"$BINARY_NAME\")" ./artifacts_version.yml)
-    if [-z "$EXISTS" ]; then
+    if [ -z "$EXISTS" ]; then
       yq e ".binaries += [{\"name\":\"$BINARY_NAME\", \"version\": 1}]" -i ./artifacts_version.yml
     else
       yq e "(.binaries[] | select(.name==\"$BINARY_NAME\").version) += 1" -i ./artifacts_version.yml
