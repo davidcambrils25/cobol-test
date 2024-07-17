afea40d8fbf84de6f95a0638e5b6c01e8f630d06
drsalas_indra <drsalas@minsait.com>
2024-07-17 14:08:52 +0200

    tests

--- cicd_scripts/git-diff.sh
+++ cicd_scripts/git-diff.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-#BINARY_NAME=$1
-
 echo $BINARY_NAME
 
 CHANGED_FILES=$(git show --pretty="" --name-only HEAD)
@@ -9,11 +7,16 @@ cd ..
 for file in $CHANGED_FILES; do
   if [[ "$file" == *".cbl"* ]]; then
     BINARY_NAME=$(basename "$file" .cbl)
-    EXISTS=$(yq e ".binaries [] | select(.name==\"$BINARY_NAME\")" ./artifacts_version.yml)
-    if [ -z "$EXISTS" ]; then
+    # Extract the current version number
+    CURRENT_VERSION=$(yq e ".binaries[] | select(.name==\"$BINARY_NAME\").version" ./artifacts_version.yml)
+    if [ -z "$CURRENT_VERSION" ]; then
+      # If the binary does not exist, add it with version 1
       yq e ".binaries += [{\"name\":\"$BINARY_NAME\", \"version\": 1}]" -i ./artifacts_version.yml
     else
-      yq e "(.binaries[] | select(.name==\"$BINARY_NAME\").version) += 1" -i ./artifacts_version.yml
+      # Increment the version number
+      NEW_VERSION=$((CURRENT_VERSION + 1))
+      # Update the version number in the YAML file
+      yq e "(.binaries[] | select(.name==\"$BINARY_NAME\").version) = \"$NEW_VERSION\"" -i ./artifacts_version.yml
     fi
   fi
-done 
+done
