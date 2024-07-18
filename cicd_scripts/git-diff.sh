250e707f659ea91da31d8190bd0362fb3c9a6f87
drsalas_indra <drsalas@minsait.com>
2024-07-18 12:39:08 +0200

    modify sh

--- cicd_scripts/git-diff.sh
+++ cicd_scripts/git-diff.sh
@@ -21,8 +21,8 @@ for file in $CHANGED_FILES; do
       # Increment the version number
       NEW_VERSION=$((CURRENT_VERSION + 1))
     fi
-    # Create new entry in the YAML file
-    yq e "(.binaries[] | select(.name == \"$BINARY_NAME\" and .version == \"$NEW_VERSION\")).sources += [\"$SOURCE_FILE\"]" -i ./artifacts_version.yml
+    # Add a new entry for the binary with the new version
+    yq e ".binaries += [{name: \"$BINARY_NAME\", version: \"$NEW_VERSION\", sources: []}]" -i ./artifacts_version.yml
   else
     NON_COBOL_FILES+=("$file")
   fi
