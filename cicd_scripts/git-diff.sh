276c1f477dcf9bcab645b863e4b4f0b84cc4d564
drsalas_indra <drsalas@minsait.com>
2024-07-18 09:59:34 +0200

    sources

--- cicd_scripts/git-diff.sh
+++ cicd_scripts/git-diff.sh
@@ -3,7 +3,6 @@
 echo $BINARY_NAME
 
 CHANGED_FILES=$(git diff --name-only HEAD~1 HEAD)
-cat $CHANGED_FILES
 cd ..
 for file in $CHANGED_FILES; do
   if [[ "$file" == *".cbl"* ]]; then
@@ -13,12 +12,21 @@ for file in $CHANGED_FILES; do
     if [ -z "$CURRENT_VERSION" ]; then
       # If the binary does not exist, add it with version 1
       yq e ".binaries += [{\"name\":\"$BINARY_NAME\", \"version\": 1}]" -i ./artifacts_version.yml
+      #yq e ".binaries += [{\"sources\":}]" -i ./artifacts_version.yml
     else
       # Increment the version number
       NEW_VERSION=$((CURRENT_VERSION + 1))
       # Update the version number in the YAML file
       yq e "(.binaries[] | select(.name==\"$BINARY_NAME\").version) = \"$NEW_VERSION\"" -i ./artifacts_version.yml
     fi
+  else
+    SOURCE_FILE=$(basename "$file")
+    # Use yq to add the file's basename to the sources array of the identified binary (for cobol dependency files)
+    yq e "(.binaries[] | select(.name == \"$BINARY_NAME\").sources) += [\"$SOURCE_FILE\"]" -i ./artifacts_version.yml
   fi
 done
 cat ./artifacts_version.yml
+
+
+#TODO
+#hacer el push de los cambios
